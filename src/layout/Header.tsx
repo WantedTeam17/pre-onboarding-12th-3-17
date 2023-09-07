0b7118fb32b00b54f5f3915c35ebf3531e31f3c6
hyebeen byun <hyebeen2658@naver.com>
2023-09-07 20:11:07 +0900

    Design: Header 디자인 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,32 +1,58 @@
 import { styled } from "styled-components";
 import { colors } from "../constants/colors";
-import Logo from "../assets/logo.svg";
+import Logo from "/public/assets/logo.svg";
 
 const Header = () => {
   return (
-    <HeaderContainer>
-      <img src={Logo} style={{margin: '0 0.5rem 0 1.2rem'}}/>
-      <h2>한국임상정보</h2>
-    </HeaderContainer>
+    <HeaderWrapper>
+      <button>
+        <img src={Logo} alt="한국 임상정보 로고" height={36} />
+        <A11yHidden>한국 임상정보</A11yHidden>
+      </button>
+      <ul>
+        <li>소개</li>
+        <li>질문과 답변</li>
+        <li>소식받기</li>
+        <li>제휴/문의</li>
+      </ul>
+    </HeaderWrapper>
   );
 };
 
 export default Header;
 
-const HeaderContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 4.3rem;
-  display: flex;
-  align-items: center;
-  background-color: ${colors.white};
-
-  @media (max-width: 600px) {
-    position: relative;
-    visibility: hidden;
-    height: 0;
-  }
+export const A11yHidden = styled.h1`
+  overflow: hidden;
+  position: absolute !important;
+  clip: rect(0, 0, 0, 0);
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
 `;
 
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 20px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: ${colors.white};
+
+  & > ul {
+    display: flex;
+    padding-right: 20px;
+
+    & > li {
+      margin-left: 30px;
+      font-weight: 600;
+      cursor: pointer;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
